139739f14372dd046403d47336245ec9b247f43d
Skyler Fokine <skylerfokine@gmail.com>
2026-04-15 14:58:05 -0400

    finished up scraping logic for ohio.edu need to finish logging for agent

--- server/app/agents/scraper.py
+++ server/app/agents/scraper.py
@@ -1,46 +1,106 @@
 import requests
 from bs4 import BeautifulSoup
-form datetime import date
+from datetime import date, datetime, timezone
+
+
+
+def scrape(url): 
+    if url == 'ohiouni': 
+        return scrape_ohio_events()
+    else: 
+        pass
 
 def scrape_ohio_events():
     #paginates, calls both functions, returns a list of raw HTML strings
-    first_of_next_month = calculate_first_of_next_month() 
+  
     page = 1
     all_links = []
     while True: 
-        links, stop = get_event_links(ohio_events_cal)
+
+        if page == 1: 
+            #Ohio university Calendar URL 
+            ohio_events_link = 'https://calendar.ohio.edu/calendar'
+        else: 
+            ohio_events_link = f'https://calendar.ohio.edu/calendar/{page}'
+
+        links, stop = get_event_links(ohio_events_link)
         all_links.extend(links)
         page += 1
         if stop:
             break
         
     #Pass links into the event html
-    events =get_event_html(links)
+    return get_event_html(all_links)
 
 
 def get_event_links(page_url):
+
+    #Grabs timestamp for logging and opens file to log 
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M")
     # Takes a listing page URL
-    #checks date of last card in request
+    try: 
+        ohio_events_cal = requests.get(page_url)
+        ohio_events_cal.raise_for_status()
+        with open("server/app/logs/scraper.log", "a") as f:
+            f.write(f"{timestamp} | Scraper Agent | [PASS] | {page_url}\n")
+
+    except  requests.exceptions.RequestException as e:
+        with open("server/app/logs/scraper.log", "a") as f: 
+            f.write(f"{timestamp} | Scraper Agent | [FAIL] | {page_url} | {str(e)}\n")
+
+
+    #All the html from the ohio university calendar page
+    ohio_events =  BeautifulSoup(ohio_events_cal.text, 'html.parser')
+
+    #Variable Setup for date check
+    current_month = date.today().month
+    current_year = date.today().year
+    now = datetime.now(timezone.utc)
+    event_links = []
+    stop = False
+
+    
+    #Event Population of all divs 
+    events = ohio_events.find_all("div", class_="em-card") 
+
+    #Checks Date of each card and ensures it is in same year and 
+    for event in events:
+        #Get our date
+        time_tag = event.find("em-local-time")
+        if not time_tag:
+            continue
+        start = time_tag.get("start")
+        if not start:
+            continue
+        event_date = datetime.fromisoformat(start)
+
+        #Filter the Current Month 
+        if event_date.month != current_month or event_date.year != current_year:
+            stop = True 
+            continue
+
+        #Filter active and non active events
+        if event_date < now: 
+            continue
+
+        # Get event page Link (Use the Title link only) 
+        title_link = event.find("h3", class_="em-card_title").find("a")
+
+        if title_link:
+            event_links.append(title_link["href"])
+
     # Returns a list of event detail URLs
 
+    return event_links, stop
 
-def get_event_html(events_url):
+def get_event_html(events_links):
+    #List to store html 
+    events_html = []
     #fetch raw HTML for a single event detail page
+    for link in events_links: 
+        fetch_event = requests.get(link)
+        events_html.append(fetch_event.text)
 
-def calculate_first_of_next_month(): 
-    today = date.today()
-
-    if today.month == 12:
-        first_of_next_month = date(today.year + 1, 1, 1)
-    else: 
-        first_of_next_month = date (today.year, today.month + 1, 1)
-    return first_of_next_month
-
-
-#Ohio university Calendar URL 
-ohio_events_cal = requests.get('https://calendar.ohio.edu/calendar')
-
-#All the html from the ohio university calendar page
-ohio_events =  BeautifulSoup(ohio_events_cal.text, 'html.parser')
-
+    #All the html from the ohio university calendar page will then need passed to parser!
+    return events_html
 
